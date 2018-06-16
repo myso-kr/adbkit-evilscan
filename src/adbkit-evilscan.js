c430c0432d62af94850f606138471709a2980fd7
Choi Won <help@myso.kr>
2018-06-16 19:16:34 +0900

    no message

--- src/adbkit-evilscan.js
+++ src/adbkit-evilscan.js
@@ -15,22 +15,11 @@ const NetScannerPromise = ((options) => {
   });
 });
 
-import ADB from 'adbkit';
 import Client from 'adbkit/lib/adb/client';
-
-const PORT_BASE = 15037;
-
 const trackDevices = Client.prototype.trackDevices;
 Client.prototype.trackDevices = function(callback) {
-  this.trackClients = this.trackClients || {};
   return Promise.resolve().then(async () => {
     const tracker = await trackDevices.apply(this);
-    const handler = async (network, port) => {
-      const adb = this.trackClients[network.ip] = this.trackClients[network.ip] || ADB.createClient(port);
-      return adb.connect(network.ip, network.port)
-      .then((device) => tracker.emit('add', _.assign({ adb }, network, device)))
-      .catch(()=>{})
-    }
     const repeater = () => {
       const networks = _.reduce(OS.networkInterfaces(), (o, networks) => {
         const network = _.find(networks, { family: 'IPv4', internal: false });
@@ -38,13 +27,13 @@ Client.prototype.trackDevices = function(callback) {
       }, []);
       Promise.map(networks, async (subnet) => {
         const devices = await NetScannerPromise({ target: subnet, port: '5555', status: 'O' });
-        return Promise.map(devices, (device, offset) => {
-          if(device.status == 'open') return handler(device, PORT_BASE + offset);
+        return Promise.map(devices, (device) => {
+          if(device.status == 'open') return this.connect(device.ip, device.port).catch(()=>{})
         });
       })
       .finally(() => setTimeout(repeater, 5000));
     }
-    setTimeout(() => Promise.map(this.listDevices(), (device, offset) => handler(device, PORT_BASE + offset)).then(() => repeater()));
+    setTimeout(() => Promise.map(this.listDevices(), (device) => tracker.emit('add', device)).then(() => repeater()));
     return tracker;
   }).nodeify(callback);
 }
